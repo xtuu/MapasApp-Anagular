4dd60915a752b380a5d7fbcde356fe214673e32f
Adrian Fernandez <adrianxtuu@gmail.com>
2022-12-07 11:18:10 -0400

    integracion de mapbox

--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-full-screen',
   templateUrl: './full-screen.component.html',
+  styles: [
+    `
+      #mapa {
+        width: 100%;
+    height: 100%;
+      }
+    `
+  ]
 })
 export class FullScreenComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+
+
+    (mapboxgl as any).accessToken = environment.mapboxToke;
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
+
+
   }
 
 }

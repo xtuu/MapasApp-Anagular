2811ce02289e5b015c99df50965fb78e50872c5c
Adrian Fernandez <adrianxtuu@gmail.com>
2022-12-08 10:15:46 -0400

    [NEW] Marcadores

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,12 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
+  styles: [
+    `
+      .mapa-container {
+        width: 100%;
+        height: 100%;
+      }
+
+
+    `
+  ]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+
+
+  @ViewChild('mapa') divMapa!: ElementRef
+
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [number, number] = [-67.03201136408785, 10.600235811602637]
 
   constructor() { }
+
+
+  ngAfterViewInit(): void {
+
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: 15
+    });
+
+  }
 
   ngOnInit(): void {
   }
